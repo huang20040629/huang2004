32a85c1d33038bd9dd5d60b03dc6032328cfbdda
huang20040629 <2178214857@qq.com>
2024-09-10 23:03:11 +0800

    Delete untitled directory

--- untitled/untitled1/src/App.java
+++
@@ -1,9 +0,0 @@
-import com.txws.ui.GameJFame;
-
-public class App {
-    public static void main(String[] args) {
-        new GameJFame();
-        //new LoginJFame();
-        //new registerJFame();
-    }
-}
